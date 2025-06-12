d14affc513d5974d5c184eff13af2f8d1ef4dcbf
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-12 23:31:02 +0530

    JD Parsing adding

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+######## JD Parsing ##############
+input_path = r"jd_data\job_description.csv"
+output_path = r"agents_outputs\jobs_summary_extracted.csv"
+
+df = load_csv(input_path)
+final_df = process_job_descriptions(df)
+save_to_csv(final_df, output_path)
+
+
 ######## CV #########
 from CVParsingAgent.CVExtractor_Agent import process_all_pdfs
 
